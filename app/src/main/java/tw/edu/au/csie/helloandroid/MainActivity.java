85234e3b05b24722a638292f219a1d4b047b6206
Wei-Tsung Su <ellington.su@gmail.com>
2016-10-21 19:06:06 +0800

    Add discussion code

--- app/src/main/java/tw/edu/au/csie/helloandroid/MainActivity.java
+++ app/src/main/java/tw/edu/au/csie/helloandroid/MainActivity.java
@@ -2,12 +2,31 @@ package tw.edu.au.csie.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText vEtName, vEtId, vEtPassword;
+    Button vBtOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vEtId = (EditText)findViewById(R.id.et_id);
+        vBtOk = (Button)findViewById(R.id.et_name);
+
+        vBtOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mName = vEtName.getText().toString();
+                Toast.makeText(MainActivity.this, "Hello, " + mName, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
